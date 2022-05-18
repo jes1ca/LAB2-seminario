5525b7447134227553350708a68359ed4ba24cab
jes1ca <ycikale.77@gmail.com>
2022-05-18 16:02:06 -0400

    feat(common): config new server

---
+++ src/App.ts
@@ -0,0 +1,33 @@
+import dotevn from "dotenv";
+import express, {Express} from "express";
+
+dotevn.config();
+
+class App {
+    private app: Express;
+    private port: number;
+    constructor() {
+        this.app = express();
+        this.port = Number(process.env.PORT) || 8000;
+        this.configure();
+        this.startModules();
+    }
+    private configure() {
+        this.app.use(express.json());
+        this.app.use(express.urlencoded());
+    }
+    private startModules() {
+        console.log("Load Modules!");
+        //codigo de los diferentes modulos del proyecto
+        this.app.use("/",(request, response) => {
+            response.send("HOLA MI PRIMER SERVIDOR de jessi");
+        })
+    }
+    public getApp() {
+        return this.app;
+    }
+    public getPort() {
+        return this.port;
+    }
+}
+export default App;
